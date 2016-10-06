92e1dc708df72a8a27af1b4cc277966d00071802
kimber <kimber.gee@gmail.com>
2016-10-05 21:03:55 -0400

    refactor timer and add styling

--- app/scripts/directives/timer.js
+++ app/scripts/directives/timer.js
@@ -7,25 +7,26 @@
       scope: { },
       link: function(scope, elemnt, attributes) {
 
-        var interval, incrementTimer, actions;
-        actions = { start: "Start", stop: "Stop" };
+        var interval, incrementTimer;
 
+        scope.buttonText = "Start"
+        scope.isActive = false;
         scope.workTimer = 1500;
-        scope.action = actions.start;
 
         incrementTimer = function() {
           scope.workTimer = scope.workTimer - 1;
         };
 
         scope.toggle = function() {
-          if (scope.action == actions.start) {
-            scope.action = actions.stop;
+          if (scope.isActive == false) {
+            scope.buttonText = "Stop"
             interval = $interval(incrementTimer, 1000, 1500);
-            console.log(interval)
-          } else if (scope.action == actions.stop) {
-            scope.action = actions.start;
+            scope.isActive = true;
+          } else if (scope.isActive == true) {
+            scope.buttonText = "Start"
             $interval.cancel(interval);
             scope.workTimer = 1500;
+            scope.isActive = false;
           }
         };
       }
