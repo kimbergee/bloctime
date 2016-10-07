e430a2294da5b08243a9867decc34e95cfbed397
kimber <kimber.gee@gmail.com>
2016-10-07 10:48:20 -0400

    add break timer and restyle

--- app/scripts/directives/timer.js
+++ app/scripts/directives/timer.js
@@ -1,5 +1,5 @@
 (function() {
-  function timer($interval) {
+  function timer($interval, MY_TIMES) {
     return {
       templateUrl: '/templates/directives/timer.html',
       replace: true,
@@ -9,24 +9,52 @@
 
         var interval, incrementTimer;
 
-        scope.buttonText = "Start"
+        scope.onBreak = false;
         scope.isActive = false;
-        scope.workTimer = 1500;
+        scope.buttonText = "Start";
+        scope.message = "Time to work!";
+        scope.currentTime = MY_TIMES.work;
 
         incrementTimer = function() {
-          scope.workTimer = scope.workTimer - 1;
+          scope.currentTime--;
+
+          if (scope.currentTime <= 0) {
+            if (!scope.onBreak) {
+              scope.currentTime = MY_TIMES.break;
+              scope.onBreak = true;
+              scope.message = "Take a break!";
+            } else {
+              scope.currentTime = MY_TIMES.work;
+              scope.onBreak = false;
+              scope.message = "Time to work!";
+            }
+            resetTimer();
+          };
+        };
+
+        var startTimer = function() {
+          interval = $interval(incrementTimer, 1000);
+          scope.isActive = true;
+          scope.buttonText = "Stop";
+        };
+
+        var resetTimer = function() {
+          $interval.cancel(interval);
+          scope.isActive = false;
+          scope.buttonText = "Start";
+
+          if (scope.onBreak) {
+            scope.currentTime = MY_TIMES.break;
+          } else {
+            scope.currentTime = MY_TIMES.work;
+          }
         };
 
         scope.toggle = function() {
-          if (scope.isActive == false) {
-            scope.buttonText = "Stop"
-            interval = $interval(incrementTimer, 1000, 1500);
-            scope.isActive = true;
-          } else if (scope.isActive == true) {
-            scope.buttonText = "Start"
-            $interval.cancel(interval);
-            scope.workTimer = 1500;
-            scope.isActive = false;
+          if (!scope.isActive) {
+            startTimer();
+          } else {
+            resetTimer();
           }
         };
       }
@@ -35,5 +63,9 @@
 
   angular
     .module('bloctime')
-    .directive('timer', timer);
+    .directive('timer', timer)
+    .constant("MY_TIMES", {
+      "work": 1500,
+      "break": 300
+    });
 })();
